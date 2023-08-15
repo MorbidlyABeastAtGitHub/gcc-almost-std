7d75d4f7fd1da7e596d8a97ba56969bbe312ae1d
MorbidlyABeastAtGitHub <142131668+MorbidlyABeastAtGitHub@users.noreply.github.com>
2023-08-14 21:41:26 -0400

    Updated stdbit.h

--- stdbit.h
+++ stdbit.h
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 bool readbit(unsigned long long foo,int index){return(foo&(1<<index));}
-unsigned long long writebit(unsigned long long foo,int index,bool boo){if(boo&&!(foo&(1<<index)))foo+=1<<index;if(!boo&&(foo&(1<<index)))foo-=1<<index;return foo;}
+void writebit(unsigned long long *foo,int index,bool boo){if(boo&&!(*foo&(1<<index)))*foo+=1<<index;if(!boo&&(*foo&(1<<index)))*foo-=1<<index;}
 unsigned long long readbits(unsigned long long foo,int start,int end){unsigned long long one=0;for(int i=start;i<end;i++)if (foo&(1<<i))one+=1<<i;return one;}
-unsigned long long writebits(unsigned long long foo,int start,int end,bool boo){for(int i=start;i<end;i++){if(boo&&!(foo&(1<<i)))foo+=1<<i;if(!boo&&(foo&(1<<i)))foo-=1<<i;}return foo;}
+void writebits(unsigned long long *foo,int start,int end,bool boo){for(int i=start;i<end;i++){if(boo&&!(*foo&(1<<i)))*foo+=1<<i;if(!boo&&(*foo&(1<<i)))*foo-=1<<i;}}
 bool freadbit(char foofile[],int index){FILE* foo = fopen(foofile, "rb");unsigned char boo;if (foo == NULL) {perror("freadbit error");exit(-1);}fseek(foo, (index / 8), SEEK_SET);fread(&boo, 1, 1, foo);fclose(foo);boo = readbit(boo, (index % 8) - 1);return boo;}
