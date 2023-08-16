177eeb06437775379eab4030c89275032e9334aa
Morbidly A Beast <142131668+MorbidlyABeastAtGitHub@users.noreply.github.com>
2023-08-16 13:33:22 -0400

    Update stdround.h

--- stdround.h
+++ stdround.h
@@ -1,3 +1,6 @@
-#include <stddef.h>
-size_t rounddown(size_t foo,size_t val){return(foo/val)*val;}
-size_t roundup(size_t foo,size_t val){return((foo/val)*val)+val;}
+// Ignore all warnings about "incompatible pointer types."
+unsigned long long rounddown(unsigned long long foo, unsigned long long val) { return(foo / val) * val; }
+unsigned long long roundup(unsigned long long foo, unsigned long long val){ return (( foo / val) * val) + val; }
+/*This all works because of integer truncation: e.g., dividing 25 by 8 results in 3.125, truncated to 3 in C.
+Then multiplying the truncated value by the first step's divisor results in a rounded value. So, 25 --> 3.125 (truncated to 3) --> 24.
+Rounding up is just taking the rounded-down value and adding the divisor's value to it.*/
